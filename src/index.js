8de0f7e0a81a79a8128863a475a73d772dcbc344
Thomas Adams <tqsare@gmail.com>
2020-07-01 11:32:24 -0700

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import fruits from './fruits';
+import {choice, remove} from './helpers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let fruit = choice(fruits);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(fruit, fruits);
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`)
